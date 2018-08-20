fa175e3073afe0f8bd6d741602ff07a38b0c0ad6
SirJUST <theoperaking@yahoo.com>
2018-08-19 20:03:50 -0700

    two 7kyu algorithms

---
+++ 7kyu_checkFlush/7kyu_checkFlush/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7kyu_checkFlush
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cards1 = new string[] {"AS", "3S", "9S", "KS", "4S"};
+            string[] cards2 = new string[] { "AD", "4S", "7H", "KS", "10S" };
+            Console.WriteLine(CheckIfFlush(cards1));
+            Console.WriteLine(CheckIfFlush(cards2));
+            Console.Read();
+        }
+
+        public static bool CheckIfFlush(string[] cards)
+        {
+            string[] options = new string[] { "S", "H", "C", "D" };
+            string firstCard = cards[0];
+            bool isFlush = true;
+            foreach (string s in options)
+            {
+                if (firstCard.Contains(s)){
+                    foreach(string st in cards)
+                    {
+                        if (!st.Contains(s)){
+                            isFlush = false;
+                            break;
+                        }
+                    }
+                }
+                else { continue; }
+            }
+            return isFlush;
+        }
+    }
+}
---
+++ ReverseNumber/ReverseNumber/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumber
+{
+    // of note: since the output is integer, if the number ends in a zero it will cut it off
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseNumber(-230001));
+            Console.ReadLine();
+        }
+
+        static public int ReverseNumber(int n)
+        {
+            bool negative = false;
+            if (n < 0)
+            {
+                n *= -1;
+                negative = true;
+            }
+            var splitUp = n.ToString().ToCharArray();
+            int totalChars = splitUp.Length;
+            char[] reversed = new char[totalChars];
+            foreach(char c in splitUp)
+            {
+                reversed[totalChars-1] = c;
+                totalChars--;
+            }
+            string almostThere = string.Join("", reversed);
+            int absoluteValue = int.Parse(almostThere);
+            int complete = absoluteValue;
+            if (negative)
+            {
+                complete *= -1;
+            }
+            return complete;
+        }
+    }
+}
