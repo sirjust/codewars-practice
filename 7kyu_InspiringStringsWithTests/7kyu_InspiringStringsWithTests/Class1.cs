ad02e67b9deb5de86c1cbac7fa92b3ac997dd77c
SirJUST <theoperaking@yahoo.com>
2018-08-26 15:50:39 -0700

    algorithm with test class

---
+++ 7kyu_InspiringStringsWithTests/7kyu_InspiringStringsWithTests/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7kyu_InspiringStringsWithTests
+{
+    public class Class1
+    {
+        public static string LongestWord(string stringOfWords)
+        {
+            string[] arrayOfWords = stringOfWords.Split(' ');
+            string longestWord = arrayOfWords[0];
+            for(int i = 1; i < arrayOfWords.Length; i++)
+            {
+                if (arrayOfWords[i].Length >= longestWord.Length)
+                {
+                    longestWord = arrayOfWords[i];
+                }
+            }
+            return longestWord;
+        }
+    }
+}
---
+++ 7kyu_InspiringStringsWithTests/7kyu_InspiringStringsWithTests/NUnitTestClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace _7kyu_InspiringStringsWithTests
+{
+    class NUnitTestClass
+    {
+        [TestFixture]
+        public class SolutionTest
+        {
+
+            private static object[] Basic_Test_Cases = new object[]
+            {
+      new object[] {"a b c d e fgh", "fgh"},
+      new object[] {"one two three", "three"},
+      new object[] {"red blue grey", "grey"},
+            };
+
+            [Test, TestCaseSource(typeof(SolutionTest), "Basic_Test_Cases")]
+            public void Basic_Test(string test, string expected)
+            {
+                Assert.AreEqual(expected, Class1.LongestWord(test));
+            }
+
+            // Note: Random tests output user code execution time
+        }
+    }
+}
