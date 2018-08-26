1afc849dc0709298faf47750fb5818e228a073a0
SirJUST <theoperaking@yahoo.com>
2018-08-26 16:46:34 -0700

    algorithm with test class

---
+++ 7kyu_ProductArray/7kyu_ProductArray/NUnitTestClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace _7kyu_ProductArray
+{
+    class NUnitTestClass
+    {
+        [TestFixture]
+        class Tests
+        {
+            [TestCase("12 20", "20 12")]
+            [TestCase("3 27 4 2", "216 24 162 324")]
+            [TestCase("13 10 5 2 9", "900 1170 2340 5850 1300")]
+            [TestCase("16 17 4 3 5 2", "2040 1920 8160 10880 6528 16320")]
+            [TestCase("-1 30 1", "30 -1 -30")]
+            [TestCase("0 10 3", "30 0 0")]
+            public void BasicTest(string s, string str)
+            {
+                Assert.That(Program.ProductArray(Foo(s)), Is.EqualTo(Foo(str)));
+            }
+            int[] Foo(string s) => s.Split().Select(int.Parse).ToArray();
+        }
+    }
+}
---
+++ 7kyu_ProductArray/7kyu_ProductArray/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+// return an array where each element is the product of all elements except the 
+// corresponding one in the argument
+
+namespace _7kyu_ProductArray
+{
+    public class Program
+    {
+        public static int[] ProductArray(int[] array)
+        {
+            int[] newArray = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                int toInsert = 1;
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        toInsert *= 1;
+                    }
+                    else
+                    {
+                        toInsert *= array[j];
+                    }
+                }
+                newArray[i] = toInsert;
+            }
+            return newArray;
+        }
+
+        static void Main(string[] args) { }
+
+    }
+}
+
+
+// this works on all practice tests and all test I have come up with, including negatives and zeroes
+// the final test in codewars (which I don't have acces to) results in a fail:
+
+// Expected: -32418  But was:  47932
+// Expected: -31514  But was:  1326684368
+// Expected: -23165  But was:  -90291328
+// Expected: -1019   But was:  551623688
+
+
