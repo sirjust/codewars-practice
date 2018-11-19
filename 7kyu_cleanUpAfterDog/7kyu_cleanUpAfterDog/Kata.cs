b9d498927a83cc1b7209e9effa6c50a9a43dcedd
SirJUST <theoperaking@yahoo.com>
2018-11-18 18:04:48 -0800

    clean garden algorithm

---
+++ 7kyu_cleanUpAfterDog/7kyu_cleanUpAfterDog/Kata.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//You have stumbled across the divine pleasure that is owning a dog and a garden.
+//Now time to pick up all the cr @p! :D
+
+//Given a 2D array to represent your garden, you must find and collect all of the dog 
+//cr @p - represented by '@'.
+
+//You will also be given the number of bags you have access to (bags), and the capactity 
+//of a bag (cap). If there are no bags then you can't pick anything up, so you can ignore cap.
+
+//You need to find out if you have enough capacity to collect all the cr @p and make your 
+//garden clean again.
+
+//If you do, return 'Clean', else return 'Cr@p'.
+
+//Watch out though - if your dog is out there ('D'), he gets very touchy about being watched.
+//If he is there you need to return 'Dog!!'.
+
+//For example:
+
+//x=
+//[[_, _, _, _, _, _]
+//[_, _, _, _,@, _]
+//[@, _, _, _, _, _]]
+
+//bags = 2, cap = 2
+
+//return --> 'Clean'
+
+namespace _7kyu_cleanUpAfterDog
+{
+    public class Kata
+    {
+        public static string Crap(char[,] x, int bags, int cap)
+        {
+            var doggy = x.Cast<char>().Select(d => d).Contains('D');
+            int whatICanPickUp = bags * cap;
+
+            if (doggy)
+            {
+                return "Dog!!";
+            }
+
+            foreach (char c in x)
+            {
+                if (c == '@')
+                {
+                    whatICanPickUp--;
+                }
+                if (whatICanPickUp < 0)
+                {
+                    return "Cr@p";
+                }
+            }
+            return "Clean";
+        }
+    }
+}
---
+++ 7kyu_cleanUpAfterDog/7kyu_cleanUpAfterDog/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace _7kyu_cleanUpAfterDog
+{
+    [TestFixture]
+    class Test
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("Clean", Kata.Crap(new char[,] { { '_', '_', '_', '_' }, { '_', '_', '_', '@' }, { '_', '_', '@', '_' } }, 2, 2));
+            Assert.AreEqual("Cr@p", Kata.Crap(new char[,] { { '_', '_', '_', '_' }, { '_', '_', '_', '@' }, { '_', '_', '@', '_' } }, 1, 1));
+            Assert.AreEqual("Dog!!", Kata.Crap(new char[,] { { '_', '_' }, { '_', '@' }, { 'D', '_' } }, 2, 2));
+        }
+    }
+}
