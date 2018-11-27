239468653a60b631b90cca3d78afad972ba02263
SirJUST <theoperaking@yahoo.com>
2018-11-27 13:08:33 -0800

    new 6kyu solution

---
+++ 6kyu_areTheySame/6kyu_areTheySame/AreTheySame.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6kyu_areTheySame
+{
+    public class AreTheySame
+    {
+        public static bool comp(int[] a, int[] b)
+        {
+            if (a == null || b == null)
+            {
+                return false;
+            }
+
+            Array.Sort(a);
+            Array.Sort(b);
+
+            int length = a.Length;
+            int[] aSquared = new int[length];
+
+            if (b.Length != length)
+            {
+                return false;
+            }
+
+            for(int i = 0; i<length; i++)
+            {
+                int thisNum = a[i];
+                int numSquared = thisNum * thisNum;
+                aSquared[i] = numSquared;
+            }
+
+            for(int i = 0; i < length; i++)
+            {
+                if (aSquared[i] != b[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ 6kyu_areTheySame/6kyu_areTheySame/AreTheySame_Test.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6kyu_areTheySame
+{
+    [TestFixture]
+    public class AreTheySame_Test
+    {
+
+        [Test]
+        public void Test1()
+        {
+            int[] a = new int[] { 121, 144, 19, 161, 19, 144, 19, 11 };
+            int[] b = new int[] { 11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19 };
+            bool r = AreTheySame.comp(a, b); // True
+            Assert.AreEqual(true, r);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int[] a = new int[] { 120, 144, 19, 161, 19, 144, 19, 11 };
+            int[] b = new int[] { 11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19 };
+            bool r = AreTheySame.comp(a, b); // True
+            Assert.AreEqual(false, r);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            int[] a = new int[] { 5,4,3,2,1 };
+            int[] b = new int[] { 1,4,9,16,25 };
+            bool r = AreTheySame.comp(a, b); // True
+            Assert.AreEqual(true, r);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            int[] a = null;
+            int[] b = new int[] { 1, 4, 9, 16, 25 };
+            bool r = AreTheySame.comp(a, b); // True
+            Assert.AreEqual(false, r);
+        }
+    }
+}
