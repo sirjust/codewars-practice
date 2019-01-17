be7b51c7af61bc29aba5b1a6e95403676b9cf83c
SirJUST <theoperaking@yahoo.com>
2019-01-17 14:59:25 -0800

    line algorithm, incomplete

---
+++ 6kyu_TicketClerk/6kyu_TicketClerk/Line.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6kyu_TicketClerk
+{
+    public class Line
+    {
+        public static string Tickets(int[] peopleInLine)
+        {
+            Dictionary<int, int> bank = new Dictionary<int, int>();
+
+            bank.Add(100, 0);
+            bank.Add(50, 0);
+            bank.Add(25, 0);
+
+            int ticketPrice = 25;
+            int change;
+            foreach(int customerBill in peopleInLine)
+            {
+                bank[customerBill]++;
+     
+                change = customerBill - ticketPrice;
+                while(change > 0)
+                {
+                    try
+                    {
+                        for(int index = 0; index < bank.Count; index++)
+                        {
+                            var item = bank.ElementAt(index);
+                            var itemKey = item.Key;
+                            var itemValue = item.Value;
+                            if (itemValue>0 && change - itemKey >= 0)
+                            {
+                                change -= itemKey;
+                                itemValue--;
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        return "NO";
+                    }
+                }
+            }
+            return "YES";
+        }
+    }
+}
---
+++ 6kyu_TicketClerk/6kyu_TicketClerk/LineTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace _6kyu_TicketClerk
+{
+    [TestFixture]
+    public class LineTests
+    {
+        [Test]
+        public void Test1()
+        {
+            int[] peopleInLine = new int[] { 25, 25, 50, 50 };
+            Assert.AreEqual("YES", Line.Tickets(peopleInLine));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int[] peopleInLine = new int[] { 25, 100 };
+            Assert.AreEqual("NO", Line.Tickets(peopleInLine));
+        }
+    }
+}
