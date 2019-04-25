135e5ff79b8687f12aa19e89961d2ad7580e7627
SirJUST <theoperaking@yahoo.com>
2019-04-24 21:09:23 -0400

    3 solutions for 7kyu complementaryDNA

---
+++ ComplementaryDNA/ComplementaryDNA/DnaStrand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplementaryDNA
+{
+    class DnaStrand
+    {
+        public static string MakeComplement(string dna)
+        {
+            // my solution
+            Dictionary<char, char> correspondingChemicals = new Dictionary<char, char> { { 'A', 'T' }, { 'T', 'A' }, { 'G', 'C' }, { 'C', 'G' } };
+            string result = "";
+            foreach (char c in dna)
+            {
+                result += correspondingChemicals[c];
+            }
+            return result;
+        }
+
+        public static string MakeComplement2(string dna)
+        {
+            // one line solution
+            return dna.Replace('T', 'a').Replace('A', 't').Replace('G', 'c').Replace('C', 'g').ToUpper();
+        }
+
+        public static string MakeComplement3(string dna)
+        {
+            // linq query syntax solution
+            return String.Join("", from c in dna
+                                   select "TCGA"["AGCT".IndexOf(c)]);
+        }
+    }
+}
---
+++ ComplementaryDNA/ComplementaryDNA/Test.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplementaryDNA
+{
+
+    [TestFixture]
+    public class DnaStrandTest
+    {
+        [TestCase]
+        public void test01()
+        {
+            Assert.AreEqual("TTTT", DnaStrand.MakeComplement3("AAAA"));
+        }
+        [TestCase]
+        public void test02()
+        {
+            Assert.AreEqual("TAACG", DnaStrand.MakeComplement3("ATTGC"));
+        }
+        [TestCase]
+        public void test03()
+        {
+            Assert.AreEqual("CATA", DnaStrand.MakeComplement3("GTAT"));
+        }
+    }
+}
