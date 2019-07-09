13b7a4fc765c6307181abf20604f12cf14ce1702
SirJUST <theoperaking@yahoo.com>
2019-07-09 09:53:40 -0400

    Find stray number in array algorithm

---
+++ 7Kyu/7Kyu.Tests/StrayTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Stray;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7Kyu.Tests
+{
+    [TestFixture]
+    public class StrayTests
+    {
+        [Test]
+        public void SimpleArray1()
+        {
+            Assert.AreEqual(2, Kata.Stray(new int[] { 1, 1, 2 }));
+        }
+    }
+}
---
+++ 7Kyu/Stray/Stray.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace Stray
+{
+    public static class Kata
+    {
+        public static int Stray(int[] numbers)
+        {
+            if(numbers[0] == numbers[1])
+            {
+                return numbers.Where(c => c != numbers[0]).FirstOrDefault();
+            }
+            else
+            {
+                return numbers.Where(c => c != numbers[2]).FirstOrDefault();
+            }
+        }
+    }
+}
