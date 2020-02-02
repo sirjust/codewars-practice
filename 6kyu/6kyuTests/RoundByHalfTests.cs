d258344c5b56a3a1fc85e30f2883b9cc0a90f890
SirJUST <theoperaking@yahoo.com>
2020-02-02 08:42:27 -0500

    Round by half kata

---
+++ 6kyu/6kyuTests/RoundByHalfTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _6kyuTests
+{
+    using NUnit.Framework;
+    using RoundByHalf;
+    using System;
+
+    [TestFixture]
+    public class SolutionTest
+    {
+        private static object[] sampleTestCases = new object[]
+        {
+      new object[] {4.2, 4},
+      new object[] {4.4, 4.5},
+      new object[] {4.6, 4.5},
+      new object[] {4.8, 5},
+        };
+
+        [Test, TestCaseSource("sampleTestCases")]
+        public void SampleTest(double n, double expected)
+        {
+            Assert.AreEqual(expected, Kata.Solution(n));
+        }
+    }
+}
---
+++ 6kyu/RoundByHalf/Kata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundByHalf
+{
+    public class Kata
+    {
+        // my solution
+        public static double Solution(double n)
+        {
+            double floor = Math.Floor(n);
+            double ceiling = Math.Ceiling(n);
+            double half = ceiling - .5;
+            if (n >= ceiling - .25) return ceiling;
+            else if (n >= half - .25) return half;
+            return floor;
+        }
+
+        // this was the best solution
+        public static double Best(double n)
+        {
+            return Math.Round(n * 2) / 2;
+        }
+    }
+}
