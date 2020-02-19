0c8f50e22a062a0f2e57993d3a857528883325d0
SirJUST <theoperaking@yahoo.com>
2020-02-18 21:49:19 -0500

    First 5kyu complete

---
+++ 5kyu/5kyu.Tests/Rot13Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Rot13;
+
+namespace _5kyu.Tests
+{
+    [TestFixture]
+    public class Rot13Tests
+    {
+        [Test, Description("test")]
+        public void testTest()
+        {
+            Assert.AreEqual("grfg", Kata.Rot13("test"), String.Format("Input: test, Expected Output: grfg, Actual Output: {0}", Kata.Rot13("test")));
+        }
+
+        [Test, Description("Test")]
+        public void TestTest()
+        {
+            Assert.AreEqual("Grfg", Kata.Rot13("Test"), String.Format("Input: Test, Expected Output: Grfg, Actual Output: {0}", Kata.Rot13("Test")));
+        }
+
+        [Test, Description("Test")]
+        public void AnotherTest()
+        {
+            Assert.AreEqual("10+2 vf gjryir.", Kata.Rot13("10+2 is twelve."));
+        }
+
+        [Test, Description("Test")]
+        public void YetAnotherTest()
+        {
+            Assert.AreEqual("nopqrstuvwxyzabcdefghijklm", Kata.Rot13("abcdefghijklmnopqrstuvwxyz"));
+        }
+    }
+}
---
+++ 5kyu/Rot13/Kata.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rot13
+{
+    public static class Kata
+    {
+        public static string Rot13(string message)
+        {
+            string lowercase = "abcdefghijklmnopqrstuvwxyz";
+            string uppercase = lowercase.ToUpper();
+            int cipher = 13;
+            StringBuilder builder = new StringBuilder();
+            foreach(char c in message)
+            {
+                if (!lowercase.Contains(char.ToLower(c)))
+                    {
+                    builder.Append(c);
+                    continue;
+                };
+                bool isLower = lowercase.Contains(c);
+                int location = lowercase.IndexOf(char.ToLower(c));
+
+                int newLocation = location + cipher;
+                newLocation = newLocation >= lowercase.Length ? newLocation - lowercase.Length : newLocation;
+
+                if (isLower) builder.Append(lowercase[newLocation]);
+                else builder.Append(uppercase[newLocation]);
+            }
+            return builder.ToString();
+        }
+    }
+}
