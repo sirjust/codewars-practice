33e94ae4de43ef37edd86ab747dcff4208888506
SirJUST <theoperaking@yahoo.com>
2020-02-22 22:55:26 -0500

    6kyu string manipulation kata

---
+++ 6kyu/6kyuTests/Smile67Tests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using Smile67Kata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6kyuTests
+{
+    [TestFixture]
+    public class Smile67KataTests
+    {
+        [Test]
+        public void smile67KataTest_withoutRandom1()
+        {
+            Assert.AreEqual("cbafed", new Kata().reverseAndCombineText("abc def"));
+        }
+        [Test]
+        public void smile67KataTest_withoutRandom2()
+        {
+            Assert.AreEqual("defabcjklghi", new Kata().reverseAndCombineText("abc def ghi jkl"));
+        }
+        [Test]
+        public void smile67KataTest_withoutRandom3()
+        {
+            Assert.AreEqual("cbafed", new Kata().reverseAndCombineText("cbafed"));
+        }
+        [Test]
+        public void smile67KataTest_withoutRandom4()
+        {
+            Assert.AreEqual("trzwqfdstrteettr45hh4325543544hjhjh21lllll", new Kata().reverseAndCombineText("234hh54 53455 sdfqwzrt rtteetrt hjhjh lllll12  44"));
+        }
+        [Test]
+        public void smile67KataTest_withoutRandom5()
+        {
+            Assert.AreEqual("gffds432243fdsfdseewttf", new Kata().reverseAndCombineText("sdfsdf wee sdffg 342234 ftt"));
+        }
+    }
+}
---
+++ 6kyu/Smile67Kata/Kata.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smile67Kata
+{
+    public class Kata
+    {
+        public string reverseAndCombineText(string text)
+        {
+            if (!text.Contains(' ')) return text;
+            text = text.Replace("  ", " ");
+            List<string> separatedWords = text.Split(' ').ToList<string>();
+
+            int timesToCombine = (separatedWords.Count + 1) / 2;
+            List<string> temp = new List<string>();
+            for(int i = 0; i < timesToCombine; i++)
+            {
+                if (separatedWords.Count == 1) return separatedWords[0];
+                for(int k = 0; k < separatedWords.Count; k++)
+                {
+                    separatedWords[k] = Reverse(separatedWords[k]);
+                }
+
+                for(int j = 0; j < separatedWords.Count; j += 2)
+                {
+                    if (j == separatedWords.Count - 1) 
+                    {
+                        temp.Add(separatedWords[j]);
+                        continue;
+                    }
+                    string combinedWord = separatedWords[j] + separatedWords[j + 1];
+                    temp.Add(combinedWord);
+                }
+                separatedWords = temp;
+                temp = new List<string>();
+            }
+            return separatedWords[0];
+        }
+        private string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
